baeeba6871c1f6129820ed164229de8b070e84f0
sandptel <sandeeppatel626735@gmail.com>
2024-11-25 05:07:20 +0530

    fixed

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     hyprland.url = "github:hyprwm/Hyprland";
     
@@ -11,23 +11,39 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+  
 
   outputs = { self, hyprland, nixpkgs, home-manager }:
+  let
+      username = "roronoa"; # $USER
+      system = "x86-64-linux";  # x86_64-linux, aarch64-multiplatform, etc.
+      stateVersion = "24.11";     # See https://nixos.org/manual/nixpkgs/stable for most recent
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+
+      
+      homeDirectory = "/home/${username}";
+
+      home = (import ./home.nix {
+        inherit homeDirectory pkgs stateVersion system username;
+      });
+in
      {
-
-
+      
       nixosModules.default = import ./nixos-dots.nix;
 
-    #   homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-    #     inherit pkgs;
-        
-
-    #     modules = [
-    #       # hyprland.homeManagerModules.default
-    #       home
-    #     ];
-    #   };
-
+      # homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+      #   inherit pkgs;
+      #   modules = [
+      #     # hyprland.homeManagerModules.default
+      #     home
+      #   ];
+      # };
 
 
     };
